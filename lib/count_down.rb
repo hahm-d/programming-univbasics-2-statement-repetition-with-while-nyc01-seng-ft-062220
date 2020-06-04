6e3ae48e69b5672db5c706ae093bfb6125ff9994
Daniel Hahm <dhahm01@gmail.com>
2020-06-04 20:43:01 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,10 @@
 # Write your code here
 count = 10 
 while count >= 1 do
-  break if count == 0 
   puts count
   count -= 1
+  break if count == 0
+    break if count == 0 
 end
 
 while count == 0 do
